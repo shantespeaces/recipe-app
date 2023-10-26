b0ad301f2a7de5f339e3c6e717d4de136750bf5c
shantespeaces <shantespeaces@gmail.com>
2023-10-26 15:05:01 -0400

    créer structure component nav & search

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,19 @@
+function SearchBar() {
+  return (
+    <div className="container-sm-fluid">
+      <form className="d-flex me-2" role="search">
+        <input
+          className="form-control me-2"
+          type="search"
+          placeholder="Search"
+          aria-label="Search"
+        ></input>
+        <button className="btn btn-outline-success" type="submit">
+          Search
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default SearchBar;
