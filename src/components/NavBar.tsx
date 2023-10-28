6e28347b3db16bcdf36e76123440681eb71bc4c8
shantespeaces <shantespeaces@gmail.com>
2023-10-28 12:42:39 -0400

    ajouter hook au navBar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,60 +1,77 @@
 import SearchBar from "./SearchBar";
-
+import { useState } from "react";
 function NavBar() {
+  let items = ["Home", "Create", "Profile"];
+
+  //hook
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
+  // const handleClick = (event: MouseEvent) => console.log(event);
   return (
-    <nav className="navbar sticky-top navbar-expand-lg bg-body-tertiary">
-      <div className="container-fluid d-flex justify-content-between align-items-center">
-        <SearchBar />
-        <a className="navbar-brand mx-auto" href="#">
-          <img src="src\assets\images\logo.png" alt="the menu logo" />
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-      </div>
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-          <li className="nav-item">
-            <a
-              className="nav-link active"
-              aria-current="page"
-              href="#"
-              onClick={() => console.log("clicked")}
-            >
-              Home <span className="material-symbols-outlined">home</span>
-            </a>
-          </li>
-          <li className="nav-item">
-            <a
-              className="nav-link"
-              href="#"
-              onClick={() => console.log("clicked")}
-            >
-              Create
-              <span className="material-symbols-outlined">add_circle</span>
-            </a>
-          </li>
-          <li className="nav-item">
-            <a
-              className="nav-link"
-              href="#"
-              onClick={() => console.log("clicked")}
-            >
-              Profile
-              <span className="material-symbols-outlined">account_circle</span>
-            </a>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <>
+      <nav className="navbar sticky-top navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid d-flex justify-content-between align-items-center">
+          <SearchBar />
+          <a className="navbar-brand mx-auto" href="#">
+            <img src="src\assets\images\logo.png" alt="the menu logo" />
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+        </div>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            {items.map((item, index) => (
+              <li
+                key={item}
+                onClick={() => {
+                  setSelectedIndex(index);
+                }}
+                className={
+                  selectedIndex === index ? "nav-item active" : "nav-item"
+                }
+              >
+                <a className="nav-link " aria-current="page" href="#">
+                  {item} <span className="material-symbols-outlined">home</span>
+                </a>
+              </li>
+            ))}
+
+            {/* <li className="nav-item">
+              <a
+                className="nav-link"
+                href="#"
+                onClick={() => console.log("clicked")}
+              >
+                Create
+                <span className="material-symbols-outlined">add_circle</span>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                className="nav-link"
+                href="#"
+                onClick={() => console.log("clicked")}
+              >
+                Profile
+                <span className="material-symbols-outlined">
+                  account_circle
+                </span>
+              </a>
+            </li> */}
+          </ul>
+        </div>
+      </nav>{" "}
+    </>
   );
 }
 export default NavBar;
