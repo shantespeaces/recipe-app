6eb77784c81094a3720a16252b9fe91efb2e1a6d
shantespeaces <shantespeaces@gmail.com>
2023-11-11 16:43:56 -0500

    dynamic checkbox prop

--- src/components/forms/CheckBox.tsx
+++ src/components/forms/CheckBox.tsx
@@ -1,21 +1,49 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
 interface CheckBoxProps {
+  title: string;
+  endpoint: string;
+}
+interface Option {
+  id: number;
   name: string;
 }
-function CheckBox({ name }: CheckBoxProps) {
+
+type Options = Option[];
+
+function CheckBox({ title, endpoint }: CheckBoxProps) {
+  const [options, setOptions] = useState<Options>([]);
+
+  useEffect(() => {
+    axios
+      .get(endpoint)
+      .then((response) => setOptions(response.data))
+      .catch((error) => console.error("Error fetching options:", error));
+  }, [endpoint]);
   return (
     <>
+      <h3>{title}</h3>
       <div className="form-check">
-        <input
-          className="form-check-input"
-          type="checkbox"
-          value=""
-          id="flexCheckDefault"
-        />
-        <label className="form-check-label" htmlFor="flexCheckDefault">
-          {name}
-        </label>
+        {options.map((option) => (
+          <div key={option.id}>
+            <input
+              className="form-check-input"
+              type="checkbox"
+              value={option.id}
+              id={`flexCheckDefault_${option.id}`}
+            />
+            <label
+              className="form-check-label"
+              htmlFor={`flexCheckDefault_${option.id}`}
+            >
+              {option.name}
+            </label>
+          </div>
+        ))}
       </div>
     </>
   );
 }
+
 export default CheckBox;
