8d67b042c685001332ed97e55474930ec6819ff8
shantespeaces <shantespeaces@gmail.com>
2023-11-30 12:33:35 -0500

    condition for hiding nav and header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 // import { useState } from "react";
 import Create from "./pages/Create";
 import Profile from "./pages/Profile";
+import Account from "./pages/Account";
+import Connexion from "./pages/Connexion";
 import Recipe from "./pages/Recipe";
 import Home from "./pages/Home";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
+
 import NavBar from "./components/nav/NavBar";
 import Header from "./components/Header";
 
@@ -20,17 +24,33 @@ function App() {
   //     //console.log(background_colors);
   //   });
 
+  const location = useLocation();
+
+  const [hideComponent, sethideComponent] = useState(true);
+
+  useEffect(() => {
+    sethideComponent(!["/connexion", "/account"].includes(location.pathname));
+  }, [location.pathname]);
+
   return (
     <div>
-      {/* <p>{bgs.color}</p> */}
-      <NavBar />
-      <Header />
+      {hideComponent && (
+        <>
+          {/* Rendered outside the Routes */}
+          {/* <p>{bgs.color}</p> */}
+          <NavBar />
+          <Header />
+        </>
+      )}
 
+      {/* Routes for different pages */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/create" element={<Create />} />
         <Route path="/profile" element={<Profile />} />
-        <Route path="/recipe" element={<Recipe />} />{" "}
+        <Route path="/recipe" element={<Recipe />} />
+        <Route path="/account" element={<Account />} />
+        <Route path="/connexion" element={<Connexion />} />
       </Routes>
     </div>
   );
