85b0b9cf245a28a5194826570127c508717c06fe
shantespeaces <shantespeaces@gmail.com>
2023-11-30 12:32:41 -0500

    connexion component

---
+++ src/pages/Connexion.tsx
@@ -0,0 +1,77 @@
+import PostIt from "../components/PostIt";
+function Connexion() {
+  return (
+    <>
+      <section className="m-5" style={{ backgroundColor: "#eee" }}>
+        <div className=" d-flex align-items-center h-100 ">
+          <div className="container h-100">
+            <div className="row d-flex justify-content-center align-items-center h-100">
+              <div className="col-12 col-md-9 col-lg-7 col-xl-6">
+                <div className="card p-5 m-5" style={{ borderRadius: "15px" }}>
+                  <div className="card-body p-5">
+                    <h2 className="text-uppercase text-center mb-5">
+                      Sign into your account
+                    </h2>
+
+                    <form>
+                      <div className="form-outline mb-4">
+                        <input
+                          type="text"
+                          className="form-control form-control-lg rounded-5"
+                        />
+                        <label className="form-label" htmlFor="form3Example1cg">
+                          Your Name
+                        </label>
+                      </div>
+
+                      <div className="form-outline mb-4">
+                        <input
+                          type="email"
+                          className="form-control form-control-lg rounded-5"
+                        />
+                        <label className="form-label" htmlFor="form3Example3cg">
+                          Email address
+                        </label>
+                      </div>
+
+                      <div className="form-outline mb-4">
+                        <input
+                          type="password"
+                          className="form-control form-control-lg rounded-5"
+                        />
+                        <label className="form-label" htmlFor="form3Example4cg">
+                          Password
+                        </label>
+                      </div>
+
+                      <div className="pt-1 mb-4">
+                        <button
+                          className="btn btn-dark btn-lg btn-block"
+                          type="button"
+                        >
+                          Login
+                        </button>
+                      </div>
+
+                      <p className="mb-5 pb-lg-2" style={{ color: "#393f81" }}>
+                        Don't have an account?{" "}
+                        <a href="/account" style={{ color: "#393f81" }}>
+                          Register here
+                        </a>
+                      </p>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <PostIt position="right" />
+      <PostIt position="left" />
+      <PostIt position="right" />
+      <PostIt position="left" />
+    </>
+  );
+}
+export default Connexion;
